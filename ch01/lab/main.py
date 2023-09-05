a4a1da0e39d7873f1b457dca95e14d67dbe880c1
ryankurtz05 <rkurtz@binghamton.edu>
2023-09-05 17:40:09 -0400

    finish lab

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,22 @@
+import random
 
+#Part A
+weeks = 16
+print("Weeks:",weeks)
+classes = 5
+print("Classes:",classes)
+tuition = 6000
+print("Tuition:",tuition)
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+classes_per_week = 3
+print("Classes per week:" , classes_per_week)
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per class:", cost_per_class)
+
+#Part B
+myList = ('a',65,'o',"star",32)
+
+rndObj = random.choice(myList)
+
+print("\nRandom Object:",rndObj)
