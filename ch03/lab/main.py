0414a6123a4606fb9466430c6b2f6d58611fa61c
ryankurtz05 <rkurtz@binghamton.edu>
2023-09-22 18:33:40 -0400

    lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -20,6 +20,9 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
+
+
+#Race 1
 michelangelo.forward(random.randrange(1,101))
 leonardo.forward(random.randrange(1,101))
 #Reset
@@ -27,6 +30,8 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 
+
+#Race 2
 for i in range(3):
     michelangelo.forward(random.randrange(1,101))
     leonardo.forward(random.randrange(1,101))
@@ -34,12 +39,140 @@ for i in range(3):
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
+
 # PART B - complete part B here
 pygame.init()
-display = pygame.display.set_mode()
+display = pygame.display.set_mode([500,500])
 
+sideLength = 40
+xpos = 250
+ypos = 250
 points = list()
-numSides = int()
 
+#Triangle
+numSides = 3
+
+for i in range(numSides):
+    angle = 360/numSides
+    radians = math.radians(angle * i)
+    x = xpos + sideLength * math.cos(radians)
+    y = ypos + sideLength * math.sin(radians)
+    points.append([x,y])
+
+pygame.draw.polygon(display,"red",points)
+pygame.display.flip()
+
+pygame.time.wait(1000)
+
+
+
+
+#Clear screen
+display.fill("black")
+pygame.display.flip()
+
+#Square
+points = []
+numSides = 4
+
+for i in range(numSides):
+    angle = 360/numSides
+    radians = math.radians(angle * i)
+    x = xpos + sideLength * math.cos(radians)
+    y = ypos + sideLength * math.sin(radians)
+    points.append([x,y])
+
+pygame.draw.polygon(display,"blue",points)
+pygame.display.flip()
+
+pygame.time.wait(1000)
+
+
+
+
+#Clear screen
+display.fill("black")
+pygame.display.flip()
+
+#Hexagon
+points = []
+numSides = 6
+
+for i in range(numSides):
+    angle = 360/numSides
+    radians = math.radians(angle * i)
+    x = xpos + sideLength * math.cos(radians)
+    y = ypos + sideLength * math.sin(radians)
+    points.append([x,y])
+
+pygame.draw.polygon(display,"green",points)
+pygame.display.flip()
+
+pygame.time.wait(1000)
+
+
+
+
+#Clear screen
+display.fill("black")
+pygame.display.flip()
+
+#Icosagon
+points = []
+numSides = 20
+
+for i in range(numSides):
+    angle = 360/numSides
+    radians = math.radians(angle * i)
+    x = xpos + sideLength * math.cos(radians)
+    y = ypos + sideLength * math.sin(radians)
+    points.append([x,y])
+
+pygame.draw.polygon(display,"blue",points)
+pygame.display.flip()
+pygame.time.wait(1000)
+
+
+
+
+#Clear screen
+display.fill("black")
+pygame.display.flip()
+
+#Hectagon
+points = []
+numSides = 100
+
+for i in range(numSides):
+    angle = 360/numSides
+    radians = math.radians(angle * i)
+    x = xpos + sideLength * math.cos(radians)
+    y = ypos + sideLength * math.sin(radians)
+    points.append([x,y])
+
+pygame.draw.polygon(display,"white",points)
+pygame.display.flip()
+pygame.time.wait(1000)
+
+
+
+
+#Clear screen
+display.fill("black")
+pygame.display.flip()
+
+#Circle
+points = []
+numSides = 360
+
+for i in range(numSides):
+    angle = 360/numSides
+    radians = math.radians(angle * i)
+    x = xpos + sideLength * math.cos(radians)
+    y = ypos + sideLength * math.sin(radians)
+    points.append([x,y])
+
+pygame.draw.polygon(display,"pink",points)
+pygame.display.flip()
 
 window.exitonclick()
